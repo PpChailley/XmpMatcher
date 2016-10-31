b4e0d744ad55db545919ae190a70497247880240
PPC <ppchailley@gmx.com>
2016-10-31 20:16:40 +0100

    Parsing of Exif Data seems to work, but slowly

--- XmpMatcher/XmpMatcherLib/XmpMatcher.cs
+++ XmpMatcher/XmpMatcherLib/XmpMatcher.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
-using ExifLibrary;
+using MetadataExtractor;
 using NLog;
-
+using Directory = MetadataExtractor.Directory;
 
 
 namespace gbd.XmpMatcher.Lib
@@ -16,8 +18,10 @@ namespace gbd.XmpMatcher.Lib
 
         private ICollection<FileInfo> _unsortedFiles;
 
-        private IDictionary<FileInfo, MatchingAttributes> _xmpFiles = new Dictionary<FileInfo, MatchingAttributes>();
-        private IDictionary<FileInfo, MatchingAttributes> _imgFiles = new Dictionary<FileInfo, MatchingAttributes>();
+        //private IDictionary<FileInfo, MatchingAttributes> _xmpFiles = new Dictionary<FileInfo, MatchingAttributes>();
+        //private IDictionary<FileInfo, MatchingAttributes> _imgFiles = new Dictionary<FileInfo, MatchingAttributes>();
+
+        private IDictionary<MatchingAttributes, ICollection<FileInfo>> _byAttributes =  new Dictionary<MatchingAttributes, ICollection<FileInfo>>();
 
         public XmpMatcher(FileInfo[] inputFiles)
         {
@@ -52,40 +56,82 @@ namespace gbd.XmpMatcher.Lib
 
             foreach (var file in _unsortedFiles)
             {
+                MatchingAttributes attribs = null;
+
                 switch (FileDiscriminator.Process(file))
                 {
                     case FileType.Xmp:
-                        _xmpFiles[file] = GetXmpAttributes(file);
+                        attribs = GetXmpAttributes(file);
                         break;
 
                     case FileType.Image:
-                        _imgFiles[file] = GetImageAttributes(file);
+                        attribs = GetImageAttributes(file);
                         break;
 
                     case FileType.Unknown:
                     default:
                         Logger.Warn($"File '{file.Name}' is not recognized");
-                        break;
+                        continue;
                 }
+
+                if (_byAttributes.ContainsKey(attribs))
+                    _byAttributes[attribs].Add(file);
+                else
+                    _byAttributes[attribs] = new List<FileInfo>() { file };
+
             }
         }
 
         private static MatchingAttributes GetImageAttributes(FileInfo file)
         {
-            var exif = ExifFile.Read(file.FullName);
+            IReadOnlyList<Directory> dirs = ImageMetadataReader.ReadMetadata(file.FullName);
+            Directory subIfd = dirs.Single(d => d.Name.Equals("Exif SubIFD"));
+
+            var dateTag = subIfd.Tags.Single(t => t.Name.Equals("Date/Time Original")).Description;
+            var parsedExifOriginalTime = TryParseExifDateTime(dateTag, file);
 
             var attribs = new MatchingAttributes
             {
-                DateShutter = DateTime.Parse(exif.Properties[ExifTag.DateTimeOriginal].Value.ToString()),
-                FocalPlaneXResolution = exif.Properties[ExifTag.FocalPlaneXResolution].Value.ToString(),
-                FocalPlaneYResolution = exif.Properties[ExifTag.FocalPlaneYResolution].Value.ToString(),
-
+                FocalPlaneXResolution = subIfd.Tags.Single(t => t.Name.Equals("Focal Plane X Resolution")).Description,
+                FocalPlaneYResolution = subIfd.Tags.Single(t => t.Name.Equals("Focal Plane Y Resolution")).Description,
+                DateShutter = parsedExifOriginalTime
             };
 
-
+            
             Logger.Debug($"Read attribs from {file.Name} : {attribs}");
 
             return attribs;
+        }
+
+        private static DateTime TryParseExifDateTime(string dateTag, FileInfo file)
+        {
+            DateTime parsedExifOriginalTime;
+            try
+            {
+                parsedExifOriginalTime = DateTime.ParseExact(dateTag, "yyyy:MM:dd HH:mm:ss",
+                    CultureInfo.InvariantCulture);
+            }
+            catch (FormatException fe)
+            {
+                Logger.Warn($"Problem parsing dateTime '{dateTag}' for file {file.Name} ... Trying fallbacks", fe);
+                try
+                {
+                    var dateFormatReader = new Regex(@"(\d+):(\d+):(\d+)\s+(\d+):(\d+):(\d+)");
+                    var regexGroups = dateFormatReader.Match(dateTag).Groups;
+                    parsedExifOriginalTime = new DateTime(int.Parse(regexGroups[1].Value),
+                        int.Parse(regexGroups[2].Value),
+                        int.Parse(regexGroups[3].Value),
+                        int.Parse(regexGroups[4].Value),
+                        int.Parse(regexGroups[5].Value),
+                        int.Parse(regexGroups[6].Value));
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e, $"DateTime decoding failed twice '{dateTag}' for file {file.Name} ");
+                    throw;
+                }
+            }
+            return parsedExifOriginalTime;
         }
 
         private static MatchingAttributes GetXmpAttributes(FileInfo file)
